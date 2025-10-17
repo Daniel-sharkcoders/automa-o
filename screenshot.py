00e8cdecc6fe32a7eda6e46fe01b4e440eb72dc7
Daniel-sharkcoders <shark07385@sharkcoders.pt>
2025-10-17 20:05:50 +0100

    screenshot program

---
+++ screenshot.py
@@ -0,0 +1,44 @@
+import flet as ft
+import pyautogui as pg
+from time import sleep
+import datetime
+
+# Função principal do app Flet
+def main(page: ft.Page):
+    page.title = "Screenshot App"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = "#EAEBED"
+    page.window.height = 500
+    page.window.width = 400
+    page.window.resizable = False
+    page.window.maximizable = False
+
+    def screenshot(e):
+        sleep(2)
+        for i in range(int(repetir.value)):
+            sleep(int(tempo.value))
+            z = datetime.datetime.now()
+            z = int(z.timestamp())
+            z = "IMG_" + str(z)
+            screenshot = pg.screenshot(region=(0, 0, int(x.value), int(y.value)))
+            screenshot.save(z + ".png")
+        
+
+    repetir = ft.TextField(hint_text="Quantas vezes quer repetir o screenshot?", color="#006989", border_color="#A3BAC3")
+
+    tempo = ft.TextField(hint_text="Qual o intervalo de tempo? (em segundos)", color="#006989", border_color="#A3BAC3")
+
+    x = ft.TextField(label="Qual a coordenada X?", border_color="#A3BAC3")
+    y = ft.TextField(label="Qual a coordenada Y?", border_color="#A3BAC3")
+    txt = ft.Text("Clique no botão para tirar um screenshot em 2 segundos", size=20, color="#006989")
+
+    btn = ft.ElevatedButton(text="Tirar screenshot", on_click=screenshot, bgcolor="#006989", color="#EAEBED")
+
+    coluna = ft.Column(controls=[txt, x, y, repetir, tempo, ft.Row(
+        controls=[btn], spacing=20,
+    )], alignment=ft.MainAxisAlignment.CENTER)
+
+
+    page.add(coluna)
+
+ft.app(target=main)
