7dd56ae0c641468df3882eaeb3d51d0ede7007e3
Daniel-sharkcoders <shark07385@sharkcoders.pt>
2025-10-18 10:17:49 +0100

    Refactor screenshot app layout and functionality
    Refactor screenshot function and UI layout for clarity.

--- screenshot.py
+++ screenshot.py
@@ -13,9 +13,10 @@ def main(page: ft.Page):
     page.window.resizable = False
     page.window.maximizable = False
 
+    #função sreenshot
     def screenshot(e):
         sleep(2)
-        for i in range(int(repetir.value)):
+        for i in range(int(repetir.value)): #loop para repetir o  sreenshot a quantidade de vezes que o usuario pedir
             sleep(int(tempo.value))
             z = datetime.datetime.now()
             z = int(z.timestamp())
@@ -28,15 +29,26 @@ def main(page: ft.Page):
 
     tempo = ft.TextField(hint_text="Qual o intervalo de tempo? (em segundos)", color="#006989", border_color="#A3BAC3")
 
-    x = ft.TextField(label="Qual a coordenada X?", border_color="#A3BAC3")
-    y = ft.TextField(label="Qual a coordenada Y?", border_color="#A3BAC3")
+    x = ft.TextField(label="Qual a coordenada X?", border_color="#A3BAC3", color="#006989")
+    y = ft.TextField(label="Qual a coordenada Y?", border_color="#A3BAC3", color="#006989")
     txt = ft.Text("Clique no botão para tirar um screenshot em 2 segundos", size=20, color="#006989")
 
     btn = ft.ElevatedButton(text="Tirar screenshot", on_click=screenshot, bgcolor="#006989", color="#EAEBED")
 
-    coluna = ft.Column(controls=[txt, x, y, repetir, tempo, ft.Row(
-        controls=[btn], spacing=20,
-    )], alignment=ft.MainAxisAlignment.CENTER)
+    coluna = ft.Column(
+                       controls=[txt,
+                        x,
+                         y,
+                         repetir,
+                           tempo,
+                             ft.Row(
+            controls=[btn],
+              spacing=20,
+              alignment=ft.MainAxisAlignment.CENTER
+    )
+    ],
+      alignment=ft.MainAxisAlignment.CENTER,
+      horizontal_alignment=ft.MainAxisAlignment.CENTER)
 
 
     page.add(coluna)
